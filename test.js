515c080c5dd796b7cd512d1add5a7037a3a333ad
Mikayel <you@example.com>
2021-03-23 19:20:58 +0400

    Commit N1

---
+++ test.js
@@ -0,0 +1,31 @@
+require('chromedriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
+var webdriver = require('selenium-webdriver');
+var driver = new webdriver.Builder().forBrowser('chrome')
+    .build();
+//driver.get('https://frontdev.caiman.am');
+// driver.findElement(By.xpath("//*[@class='services']/span[1]")).click();
+// driver.findElement(By.xpath("//*[@class='services']/span[2]")).click();
+
+
+
+
+driver.get('https://frontdev.caiman.am');
+//driver.findElement(By.className("services"))
+
+
+// driver.sleep(5000).then(function () {
+//     driver.findElement(By.xpath("//*[@class='services']/span[1]")).click();
+//     console.log("barev-1")
+// });
+driver.sleep(5000).then(function () {
+    driver.findElement(By.xpath("//*[@class='services']/span[2]")).click();
+    console.log("barev-2")
+
+})
+driver.sleep(10000).then(function () {
+    driver.findElement(By.className("ant-btn ant-btn-primary sc-bdfBwQ imaoqE caiman-btn caiman-btn-primary")).click();
+    console.log("barev-3")
+
+})
+
