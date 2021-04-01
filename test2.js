1a2d1e4ae84d01a818c1154f26836188bcf57062
Mikayel <you@example.com>
2021-04-01 20:41:55 +0400

    countries in alphabetical

--- test2.js
+++ test2.js
@@ -1,4 +1,4 @@
-
+//Check that countries are in alphabetical order
 require('chromedriver');
 const { Builder, By, Key, until } = require('selenium-webdriver');
 var webdriver = require('selenium-webdriver');
@@ -6,7 +6,7 @@ var driver = new webdriver.Builder().forBrowser('chrome')
     .build();
 driver.get('http://demo.litecart.net/admin/login.php');
 driver.findElement(By.className('btn btn-default')).click();
-driver.sleep(5000).then(function () {
+driver.sleep(10000).then(function () {
     var countreis = driver.findElement(By.css('[title=Countries]'))
     countreis.click()
     console.log("barev-4")
@@ -19,13 +19,28 @@ driver.sleep(5000).then(function () {
 
 // })
 
-driver.sleep(10000).then(function () {
 
-    driver.findElement(By.xpath('//*[@id="content"]/div/div[3]/form/table/tbody/tr[1]/td[5]/a')).getText().then(function (txt) {
-        console.log("the" + txt)
-    });
+//get text and console log
+driver.sleep(20000).then(function () {
+
+    // driver.findElement(By.xpath('//*[@id="content"]/div/div[3]/form/table/tbody/tr[1]/td[5]/a')).getText().then(function (txt) {
+    //     console.log(txt)
+    // });
+    driver.findElements(By.xpath('/html/body/div/main/div/div/div[3]/form/table/tbody/tr/td[5]')).then(function (txt) {
+        const countries = txt.map(el => el.getText().then(function (ttext) { return (ttext) }))
+        Promise.all(countries).then(values => {
+            const countriesName = values;
+            const countriesNameSort = values.sort();
+            function array_compare(countriesName, countriesNameSort) {
+                for (i = 0; i < countriesName.length; i++)
+                    if (countriesName[i] != countriesNameSort[i])
+                        return false;
+                return true;
+            }
+            console.log(array_compare(countriesName, countriesNameSort))
+        });
+    })
 })
-
 
 
 
