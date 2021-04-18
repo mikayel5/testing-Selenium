8c0f326ca612ec9c8feb6603782584060db89c37
mikayel5 <mikayelnahapetyan@gmail.com>
2021-04-19 01:19:49 +0400

    screenshots

--- test5.js
+++ test5.js
@@ -1,29 +1,29 @@
 //require('chromedriver');
 require('geckodriver');
 const { Builder, By, Key, until, WebDriver, WebElement, Actions } = require('selenium-webdriver');
+let fs = require('fs');
 (async function example() {
     let driver = await new Builder().forBrowser('firefox').build();
     try {
         // Navigate to Url
         await driver.get('https://essentialsln.com/');
-
+        /** click and have requiared fields */
         await driver.findElement(By.xpath('/html/body/div[1]/div[2]/div/div/div/main/article/div/div[1]/div/div/div/div/div/div[2]/div/div/div/div/div/div/div/a')).click();
         await driver.findElement(By.xpath('//html/body/div[1]/div[2]/div/div/div/main/article/div/div[13]/div/div/div/div/div/div/div[2]/div/div/div/div/div/form/div[2]/p/input')).click();
         let result = await driver.wait(until.elementLocated(By.xpath('/html/body/div[1]/div[2]/div/div/div/main/article/div/div[13]/div/div/div/div/div/div/div[2]/div/div/div/div/div/form/div[2]/div[1]/label/span/span')), 10000)
-        await driver.sleep(5000);
+        await driver.sleep(3000);
 
         if (result) {
+            /* send message*/
             await driver.findElement(By.name('First')).sendKeys('Mikayel');
             await driver.findElement(By.name('text-576')).sendKeys('Nahapetyan');
             await driver.findElement(By.name('your-email')).sendKeys('mikayel@gmail.com');
             await driver.findElement(By.name('your-message')).sendKeys('Hello my frineds');
             await driver.findElement(By.xpath('//html/body/div[1]/div[2]/div/div/div/main/article/div/div[13]/div/div/div/div/div/div/div[2]/div/div/div/div/div/form/div[2]/p/input')).click();
-            /*
-            **
-            */
 
+            /** click and show menu1 */
+            await driver.sleep(5000);
             await (await driver.findElement(By.xpath('/html/body/div[1]/header/div[1]/div/div/div[1]/div[2]/div'))).click();
-            //await driver.sleep(5000);
             const menuTexts1 = await driver.findElements(
                 By.xpath('/html/body/div[1]/header/div[1]/div/div/div[1]/div[2]/div/div/ul/li/a'),
             );
@@ -33,18 +33,18 @@ const { Builder, By, Key, until, WebDriver, WebElement, Actions } = require('sel
                 menu1.push(text);
             }
             console.log("names-1", menu1)
-            /**
-             *
-             */
-
+            /** take screensho-1 */
+            let takeScreen_1 = await driver.takeScreenshot();
+            await fs.writeFileSync('./image-1.png', takeScreen_1, 'base64');
+            /* resize window*/
             await driver.manage().window().setRect({ width: 768, height: 500 });
 
-            /**
-             * 
-             */
-            await driver.sleep(5000);
+            /** click and show menu2 */
+            await driver.sleep(3000);
             await (await driver.findElement(By.xpath('/html/body/div[1]/header/div[3]/div/div/a/i'))).click();
-
+            /**take screenshot-2 */
+            let takeScreen_2 = await driver.takeScreenshot();
+            await fs.writeFileSync('./image-2.png', takeScreen_2, 'base64');
             const menuTexts2 = await driver.findElements(
                 By.xpath('/html/body/div[7]/div/ul/li/a'),
             );
@@ -54,7 +54,7 @@ const { Builder, By, Key, until, WebDriver, WebElement, Actions } = require('sel
                 menu2.push(text);
             }
             console.log("names-2-", menu2)
-
+            /** compare menus */
             function array_compare(menuTexts1, menuTexts2) {
                 for (i = 0; i < menuTexts1.length; i++)
                     if (menuTexts1[i] != menuTexts2[i])
@@ -64,13 +64,13 @@ const { Builder, By, Key, until, WebDriver, WebElement, Actions } = require('sel
             console.log(array_compare(menuTexts1, menuTexts2))
 
         } else {
-            console.log("We have error")
+            console.log("We have some problem")
         }
 
     }
     finally {
         console.log("hell")
-        //driver.quit();
+        driver.quit();
     }
 
 })();
--- test6.js
+++ test6.js
@@ -5,7 +5,6 @@ const { Builder, webdriver, By } = require('selenium-webdriver');
     let driver = new Builder()
         .forBrowser('chrome')
         .build();
-
     await driver.get("https://frontdev.caiman.am/");
     await driver.findElement(By.xpath('//*[@id="root"]/div/div[2]/div/div[1]/div/span[2]')).click();
     await driver.sleep(3000);
@@ -23,6 +22,19 @@ const { Builder, webdriver, By } = require('selenium-webdriver');
     await driver.findElement(By.id('description')).sendKeys('anmorganmorganmorgamorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorgannmorganmorganmorganmorganmmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorgannmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorgamorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmrganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorganmorgan');
     await driver.findElement(By.xpath('/html/body/div[3]/div/div[2]/div/div[2]/div/div/form/div[2]/button[2]')).click();
     await driver.sleep(3000);
+    let netxt = await driver.findElement(By.xpath('/html/body/div[3]/div/div[2]'));
+    // Capture offset positions of element
+    let offset = await netxt.getRect();
+    let x = await offset.x;
+    let y = await offset.y;
+    const actions = driver.actions({ async: true });
+    // Performs mouse move action onto the element
+    await actions.move({ x: parseInt(10), y: parseInt(10) }).click().perform();
+    await driver.sleep(4000);
+    await driver.findElement(By.xpath('/html/body/div[6]/div/div[2]/div/div[2]/div/div/div[2]/button[2]/span')).click();
+    await driver.findElement(By.xpath('//*[@id="root"]/div/div[2]/div/div[2]/span[3]/div')).click();
+
     var errorRes = await driver.manage().logs().get("browser");
     console.log(errorRes);
+
 })();
